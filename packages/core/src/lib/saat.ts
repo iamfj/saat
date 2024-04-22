07d9ea4cec43cb5c36d2f5bcf4d0b66d8f965531
Fabian Jocks <24557998+iamfj@users.noreply.github.com>
2024-04-22 23:43:23 +0000

    feat(saat): added model name type

--- packages/core/src/lib/saat.ts
+++ packages/core/src/lib/saat.ts
@@ -1,5 +1,9 @@
+import { type Prisma } from '@prisma/client';
+
+type ModelName = keyof Prisma.TypeMap['model'];
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any -- not implemented yet
-export function saat(model: string, options: any) {
+export function saat(model: ModelName, options: any) {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-return -- not implemented yet
   return [model, options];
 }
